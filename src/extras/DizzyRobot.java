7243fb91ef836d3ae4a2bacd9b11000a4d196351
Cao.Derek <Cao.Derek@SEMS-259-14.SEMS259.LAB>
2018-03-07 14:52:41 -0800

    Dizzy Robot, FizzBuzz

--- src/extras/DizzyRobot.java
+++ src/extras/DizzyRobot.java
@@ -3,16 +3,19 @@
 
 package extras;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
 	static Robot robot = new Robot();
 	public static void main(String[] args) {
-		
+		robot.setSpeed(10);
 		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
-
+		String yes = JOptionPane.showInputDialog("How dizzy are you in a scale of 1-10?");
  // 1. Use the dance method to make the robot spin.
-	
+		int spins = Integer.parseInt(yes);
+		dance(spins);
 
 	}
 
